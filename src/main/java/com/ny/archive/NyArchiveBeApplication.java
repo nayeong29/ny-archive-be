df769dd789066541153629797b1d72fa66e134f4
nanna29 <kny@swu.ac.kr>
2026-01-24 20:22:40 -0500

    feat: #1 백엔드 초기 환경 세팅 완료

---
+++ src/main/java/com/ny/archive/NyArchiveBeApplication.java
@@ -0,0 +1,13 @@
+package com.ny.archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyArchiveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyArchiveBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ny/archive/NyArchiveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ny.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyArchiveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

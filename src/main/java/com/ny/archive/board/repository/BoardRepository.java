c66a173e3e63a56587bf672332c518726bc961e7
nanna29 <kny@swu.ac.kr>
2026-01-25 18:12:55 -0500

    feat: #3 방명록 Repository 구현

--- src/main/java/com/ny/archive/board/repository/BoardRepository.java
+++ src/main/java/com/ny/archive/board/repository/BoardRepository.java
@@ -1,4 +1,9 @@
 package com.ny.archive.board.repository;
 
-public class BoardRepository {
+import com.ny.archive.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> { // Board 가 엔티티 클래스며 그 엔티티의 ID 타입은 Long이다
 }

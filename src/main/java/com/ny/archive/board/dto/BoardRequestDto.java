884387437bd18939a0ac34d0cfed1a84ed1b8fd9
nanna29 <kny@swu.ac.kr>
2026-01-25 20:08:55 -0500

    feat: #3 DTO 구현

--- src/main/java/com/ny/archive/board/dto/BoardRequestDto.java
+++ src/main/java/com/ny/archive/board/dto/BoardRequestDto.java
@@ -1,4 +1,26 @@
 package com.ny.archive.board.dto;
 
+import com.ny.archive.board.domain.Board;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
 public class BoardRequestDto {
+
+    private String author;
+    private String content;
+    private Integer stickerId;
+    private String password;
+
+    public Board toEntity() {
+        return Board.builder()
+                .author(this.author)
+                .content(this.content)
+                .stickerId(this.stickerId)
+                .password(this.password)
+                .build();
+    }
 }
--- src/main/java/com/ny/archive/board/dto/BoardResponseDto.java
+++ src/main/java/com/ny/archive/board/dto/BoardResponseDto.java
@@ -1,4 +1,25 @@
 package com.ny.archive.board.dto;
 
+import com.ny.archive.board.domain.Board;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
 public class BoardResponseDto {
+    private Long id;
+    private String author;
+    private String content;
+    private Integer stickerId;
+    private String createdAt;
+
+    public BoardResponseDto (Board board){
+        this.id= board.getId();
+        this.author= board.getAuthor();
+        this.content= board.getContent();
+        this.stickerId= board.getStickerId();
+        this.createdAt= board.getCreatedAt()
+                .format(DateTimeFormatter.ofPattern("yy/MM/dd HH:mm"));
+    }
 }

7144fa54bf01220d816fbcaef471e835532725f6
nanna29 <kny@swu.ac.kr>
2026-01-26 04:17:24 -0500

    feat: #3 방명록 수정 api 구현

--- src/main/java/com/ny/archive/board/controller/BoardController.java
+++ src/main/java/com/ny/archive/board/controller/BoardController.java
@@ -1,4 +1,21 @@
 package com.ny.archive.board.controller;
 
+import com.ny.archive.board.dto.BoardRequestDto;
+import com.ny.archive.board.dto.BoardResponseDto;
+import com.ny.archive.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/boards/")
 public class BoardController {
+    private final BoardService boardService;
+
+    @PatchMapping("/{id}")
+    public BoardResponseDto updateBoard(@PathVariable Long id,
+                                        @RequestBody BoardRequestDto requestDto) {
+        return boardService.updateBoard(id, requestDto);
+    }
+
 }

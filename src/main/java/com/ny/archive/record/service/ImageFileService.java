c5956557d181867daebd8517a78e896c2f69bf64
nanna29 <kny@swu.ac.kr>
2026-02-15 14:26:48 -0500

    feat: #7 S3 DB 배포 및 fileservice 로컬 -> 클라우드 변경

--- src/main/java/com/ny/archive/record/service/ImageFileService.java
+++ src/main/java/com/ny/archive/record/service/ImageFileService.java
@@ -2,45 +2,62 @@ package com.ny.archive.record.service;
 
 import com.ny.archive.common.exception.CustomException;
 import com.ny.archive.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Service
+@RequiredArgsConstructor
 public class ImageFileService {
-    private final String uploadPath = "/Users/nellie/backend-dev/images/";
 
-    @Value("${file.prefix}")
-    private String prefix;
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${aws.s3.base-url}")
+    private String s3BaseUrl;
 
     public String saveFile(MultipartFile file, String imageFileKey) {
-        try {
-            // 사용자가 올린 파일 이름을 가져옴
-            String originalFilename = file.getOriginalFilename();
 
-            // 확장자 존재 확인
-            int dotIndex = originalFilename.lastIndexOf(".");
-            if (dotIndex < 0) {
-                throw new CustomException(ErrorCode.INVALID_FILE_EXTENSION);
-            }
+        if (file.getOriginalFilename() == null) {
+            throw new CustomException(ErrorCode.INVALID_FILE_EXTENSION);
+        }
 
-            // 확장자 추출
-            String extension = originalFilename.substring(dotIndex);
+        // 사용자가 올린 파일 이름을 가져옴
+        String originalFilename = file.getOriginalFilename();
 
-            // imageKey + 확장자로 이름 저장
-            String savedFilename = imageFileKey + extension;
+        // 확장자 존재 확인
+        int dotIndex = originalFilename.lastIndexOf(".");
+        if (dotIndex < 0) {
+            throw new CustomException(ErrorCode.INVALID_FILE_EXTENSION);
+        }
 
-            // 어느 폴더에 어떤 이름으로 저장할지 정하는 File 객체 만듦 (주소지 만들기)
-            File target = new File(uploadPath, savedFilename);
+        // 확장자 추출
+        String extension = originalFilename.substring(dotIndex);
 
-            // file 을 target 주소로 넣음
-            file.transferTo(target);
+        // imageKey + 확장자로 이름 저장
+        String savedFilename = "journey/" + imageFileKey + extension;
+
+        try (var inputStream = file.getInputStream()) {
+            // S3 업로드 명세서 작성
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(savedFilename)
+                    .contentType(file.getContentType())
+                    .build();
+
+            // S3으로 실제 파일 전송
+            s3Client.putObject(
+                    putObjectRequest,
+                    RequestBody.fromInputStream(inputStream, file.getSize()));
 
             // DB에 저장할 수 있게 새로 만든 파일 이름 리턴
             return savedFilename;
@@ -51,18 +68,19 @@ public class ImageFileService {
 
     public void deleteFile(String imageFileName) {
         try {
-            // 파일 이름으로 로컬 저장소 경로 찾아냄
-            Path path = Paths.get(uploadPath, imageFileName);
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(imageFileName)
+                    .build();
 
-            // 파일이 있으면 지우고 없으면 넘어감
-            Files.deleteIfExists(path);
-        } catch (IOException e) {
+            s3Client.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
             throw new CustomException(ErrorCode.FILE_DELETE_ERROR);
         }
     }
 
     public String getFullPath(String fileName) {
         if (fileName == null) return null;
-        return prefix + fileName;
+        return s3BaseUrl + fileName;
     }
 }
